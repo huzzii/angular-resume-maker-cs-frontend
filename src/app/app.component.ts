54c181531affee306cc41ad6713f5f4fbdb4d78a
huzzii <huzzii466@gmail.com>
2022-06-28 17:55:10 +0530

    custom theme added with login form ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,83 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{title}}</h1>
-    <app-abc></app-abc>
-  <app-huh></app-huh>
+    <div
+      class="overlay"
+      fxLayout="column"
+      fxLayoutAlign="center center"
+      fxLayoutGap="40px"
+    >
+      <img src="../assets/digiresume-green.png" width="20%" alt="" />
+      <mat-card fxLayout="column">
+        <h2>Login</h2>
+        <form (ngSubmit)="this.loginForm.valid && login()" [formGroup]="this.loginForm" action="" fxLayout="column">
+          <mat-form-field>
+            <input
+              formControlName="email"
+              type="email"
+              placeholder="Email"
+              matInput
+            />
+            <mat-error>Email is required</mat-error>
+          </mat-form-field>
+          <mat-form-field>
+            <input
+              formControlName="password"
+              type="password"
+              placeholder="Password"
+              matInput
+            />
+            <mat-error>(8-12 Digit) Password is required</mat-error>
+          </mat-form-field>
+          <a style="margin-top:2rem" href="">Forgot Password</a>
+          <div
+            style="margin-top:2rem"
+            fxLayout="row"
+            fxLayoutGap="15px"
+            fxLayoutAlign="end"
+          >
+            <button type="submit" color="primary" mat-raised-button>Login</button>
+            <button color="accent" mat-raised-button>Signup</button>
+          </div>
+        </form>
+      </mat-card>
+    </div>
+  `,
+  styles: [
+    `
+      .overlay {
+        width: 100%;
+        height: 100%;
+      }
+      mat-card {
+        width: 25rem;
+        height: 20rem;
+      }
+      button {
+        color: #fff !important;
+        letter-spacing: 1px;
+        text-transform: uppercase;
+      }
     `,
-  styles: [`
-  h1{
-    color: red;
-  }
-  `]
+  ],
 })
 export class AppComponent {
-  title = 'angular-course';
+  loginForm: FormGroup;
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(12),
+        Validators.minLength(8),
+      ]),
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
